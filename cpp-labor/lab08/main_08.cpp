fe07d6ef96b67a6cf5509524d24994a2efe80839
NorbertBeder <beder.norbert@gmail.com>
2022-11-12 22:21:05 +0200

    labor8

--- cpp-labor/lab08/main_08.cpp
+++ cpp-labor/lab08/main_08.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <random>
 #include <vector>
 #include <functional>
+#include <string>
 
 
 using namespace std;
@@ -14,13 +16,14 @@ int main() {
             "melon", "strawberry", "raspberry", "apple", "banana", "walnut", "lemon"
     };
 
-    string whatToFind = "straw";
+    string whatToFind = "str";
 
     auto result1 = find_if(fruits.begin(), fruits.end(), [&whatToFind](const string &f) {
         return !f.find(whatToFind);
     });
 
     cout << *result1 << endl;
+
 
     vector<int> numbers = {1, 2, 3, 4, 5};
 
@@ -29,25 +32,29 @@ int main() {
 //        return n = n + 2;
 //    });
 
-    cout << endl;
     auto result2 = all_of(numbers.begin(), numbers.end(), [](const int &e) {
         return e % 2 == 0;
     });
 
     //2.feladat
+    cout << endl << "2.feladat" << endl;
+
     cout << "Numbers: ";
     for_each(numbers.begin(), numbers.end(), [](const int &e) {
         cout << e << " ";
     });
 
-    cout << endl << "2.feladat" << endl;
-    cout << (result2 ? "Mind paros" : "Nem mind paros") << endl << endl;
+    cout << (result2 ? "-> Mind paros" : "-> Nem mind paros") << endl << endl;
 
     //3.feladat
     cout << "3.feladat" << endl;
+    for_each(numbers.begin(), numbers.end(), [](const int &e) {
+        cout << e << " ";
+    });
     for_each(numbers.begin(), numbers.end(), [](int &e) {
         e += e;
     });
+    cout << "--> ";
     for_each(numbers.begin(), numbers.end(), [](const int &e) {
         cout << e << " ";
     });
@@ -61,6 +68,10 @@ int main() {
             "July", "August", "September", "October", "November", "December"
     };
 
+    for_each(months.begin(), months.end(), [](const string &e) {
+        cout << e << " ";
+    });
+    cout << "\nLength 5: ";
     int result4 = count_if(months.begin(), months.end(), [](const string &e) {
         return e.length() == 5;
     });
@@ -70,11 +81,18 @@ int main() {
     //5.feladat
     cout << "5.feladat" << endl;
 
+    for_each(numbers.begin(), numbers.end(), [](const int &e) {
+        cout << e << " ";
+    });
+    cout << "\nDescending with greater: ";
     sort(numbers.begin(), numbers.end(), greater<int>());
+
     for_each(numbers.begin(), numbers.end(), [](const int &e) {
         cout << e << " ";
     });
     cout << endl;
+
+    cout << "Ascending with for_each: ";
 
     sort(numbers.begin(), numbers.end(), [](int a, int b) {
         return a < b;
@@ -90,6 +108,12 @@ int main() {
                                                     make_pair("March", 5), make_pair("April", 9), make_pair("May", 15),
                                                     make_pair("November", 4), make_pair("December", -2)
     };
+
+    for_each(monthsWithTemp.begin(), monthsWithTemp.end(), [](const pair<string, double> &pair) {
+        cout << pair.first << ":" << pair.second << " ";
+    });
+    cout << endl;
+
     sort(monthsWithTemp.begin(), monthsWithTemp.end(),
          [](const pair<string, double> &pair1, const pair<string, double> &pair2) {
              return pair1.second < pair2.second;
@@ -102,12 +126,41 @@ int main() {
     //7.feladat
 
     cout << "7.feladat" << endl;
-    vector<double> numbers2 {-5, -6, -12, 5, 8, 10, -25, -46, 120};
-    sort(numbers2.begin(), numbers2.end(), [](double a, double b){
+    vector<double> numbers2{-5, -6, -12, 5, 8, 10, -25, -46, 120};
+    for_each(numbers2.begin(), numbers2.end(), [](const double &e) {
+        cout << e << " ";
+    });
+    cout << "--> ";
+    sort(numbers2.begin(), numbers2.end(), [](double a, double b) {
         return abs(a) < abs(b);
     });
     for_each(numbers2.begin(), numbers2.end(), [](const double &e) {
         cout << e << " ";
+    });
+
+    //8.feladat
+
+    cout << "\n\n8.feladat" << endl;
+
+    for_each(months.begin(), months.end(), [](const string &e) {
+        cout << e << " ";
+    });
+    cout << endl;
+
+    for_each(months.begin(), months.end(), [](string &e) {
+        e[0] = tolower(e[0]);
+        cout << e << " ";
+    });
+
+    //9.feladat
+
+    cout << "\n\n9.feladat" << endl;
+
+    vector<char> abc {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                      's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    shuffle(abc.begin(), abc.end(), std::mt19937(std::random_device()()));
+    for_each(abc.begin(), abc.end(), [](const char &e){
+       cout << e << " ";
     });
 
     return 0;
